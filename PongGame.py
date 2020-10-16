10129549131fdea71cea35ab62275687e65c40f2
IAmMaulik <maulik.shah715@gmail.com>
2020-10-16 14:09:43 +0530

    Added the ball colission logic

--- PongGame.py
+++ PongGame.py
@@ -1,11 +1,27 @@
 import pygame, sys
 
+def ball_movement():
+    global ball_speed_x
+    global ball_speed_y
+
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    # making the ball bounce
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_speed_x *= -1
+
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+
 pygame.init()
 clock = pygame.time.Clock()
 
 # setting up the window
 screen_width = 1280
-screen_height = 960
+screen_height = 720
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pong")
 
@@ -17,14 +33,19 @@ opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
 bg = pygame.Color('grey12')
 light_grey = (200, 200 ,200)
 
+# Speed Variables
+ball_speed_x = 7
+ball_speed_y = 7
+
 while True:
     # taking input
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    ball_movement()
 
-    # Visuals
+    # Drawing the stuff we defined
     screen.fill(bg)
     pygame.draw.rect(screen, light_grey, player)
     pygame.draw.rect(screen, light_grey, opponent)
